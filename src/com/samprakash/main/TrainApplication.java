1791195f21bd0d79e488916f4fda3fc03fba7d6e
Sam-Prakash-M <msamprakash05@gmail.com>
2024-11-16 16:30:17 +0530

    Check

--- src/com/samprakash/main/TrainApplication.java
+++ src/com/samprakash/main/TrainApplication.java
@@ -5,7 +5,7 @@ public class TrainApplication {
 	public static void main(String[] args) {
 		
 		System.out.println("hi Hello");
-		System.out.println("Welcome to Train Ticket Booking Applications");
+		System.out.println("Welcome to Train Ticket Booking Application");
 	}
 
 }
