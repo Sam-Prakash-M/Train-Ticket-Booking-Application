190951c82c23477382b4a330536435131e444dd0
Sam-Prakash-M <148543033+Sam-Prakash-M@users.noreply.github.com>
2024-11-16 18:30:34 +0530

    Update TrainApplication.java
    Removed UnNeccessary Print Statement

--- src/com/samprakash/main/TrainApplication.java
+++ src/com/samprakash/main/TrainApplication.java
@@ -3,8 +3,6 @@ package com.samprakash.main;
 public class TrainApplication {
 
 	public static void main(String[] args) {
-		
-		System.out.println("hi Hello");
 		System.out.println("Welcome to Train Ticket Booking Applications");
 	}
 
