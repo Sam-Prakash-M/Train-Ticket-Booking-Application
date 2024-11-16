852abf6864694fc8e1d818fd733534c620189532
Sam-Prakash-M <msamprakash05@gmail.com>
2024-11-16 18:39:59 +0530

    Object Created

--- src/com/samprakash/main/TrainApplication.java
+++ src/com/samprakash/main/TrainApplication.java
@@ -4,6 +4,7 @@ public class TrainApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Train ticket Booking Application");
+		TrainApplication trainApplication = new TrainApplication();
 	}
 
 }
