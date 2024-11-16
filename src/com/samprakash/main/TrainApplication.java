7376d7b81ab3aee55060aaa67e97ecc0257ffa14
Sam-Prakash-M <msamprakash05@gmail.com>
2024-11-16 15:32:50 +0530

    Initial Commit

---
+++ src/com/samprakash/main/TrainApplication.java
@@ -0,0 +1,10 @@
+package com.samprakash.main;
+
+public class TrainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi Hello");
+	}
+
+}
