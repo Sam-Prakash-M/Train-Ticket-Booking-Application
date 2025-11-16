b5ba33fd8cf76791445e42bf6594bbb1bac2808a
Sam-Prakash-M <msamprakash05@gmail.com>
2025-11-16 15:43:05 +0530

    Log out page set up is started

---
+++ src/main/java/com/samprakash/baseview/LogoutServlet.java
@@ -0,0 +1,18 @@
+package com.samprakash.baseview;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+
+}
